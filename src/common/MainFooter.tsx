878e92c1a2a1b2fd1b5df5bd6d8f71ef089b9f7d
vdabel <132954955+vdabel@users.noreply.github.com>
2025-10-03 16:12:41 -0500

    Main footer: avance 2

--- src/common/MainFooter.tsx
+++ src/common/MainFooter.tsx
@@ -15,44 +15,44 @@ const MainFooter = () => {
     <footer>
       <div className="max-w-7xl m-auto pt-25 text-(--color-text)">
         <div className='flex flex-row pb-22'>
-
           <div className='w-1/2 flex flex-col gap-8'>
             <img src={logo} alt="logo" className='w-60 object-contain' />
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
-            <div className='columns-4'>
-              <span><a href="#" className='border border-(--color-text) rounded-[50%] p-5 size-7 grid place-content-center' ><FontAwesomeIcon icon={faFacebookF} className='text-xs' /></a></span>
-              <span><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></span>
-              <span><a href="#"><FontAwesomeIcon icon={faXTwitter} /></a></span>
-              <span><a href="#"><FontAwesomeIcon icon={faLinkedinIn} /></a></span>
+            <p className='pr-35'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <div className='flex flex-row gap-2'>
+              <a href="#" className='border border-(--color-text) rounded-[50%] size-10  grid place-content-center hover:text-(--color-hover) duration-300 ease-in'><FontAwesomeIcon icon={faFacebookF} /></a>
+              <a href="#" className='border border-(--color-text) rounded-[50%] size-10  grid place-content-center hover:text-(--color-hover) duration-300 ease-in'><FontAwesomeIcon icon={faInstagram} /></a>
+              <a href="#" className='border border-(--color-text) rounded-[50%] size-10  grid place-content-center hover:text-(--color-hover) duration-300 ease-in'><FontAwesomeIcon icon={faXTwitter} /></a>
+              <a href="#" className='border border-(--color-text) rounded-[50%] size-10  grid place-content-center hover:text-(--color-hover) duration-300 ease-in'><FontAwesomeIcon icon={faLinkedinIn} /></a>
             </div>
           </div>
-
-          <div className='w-1/2 flex flex-row'>
-            <div>
-              <h6>Links</h6>
-              <ul>
-                {navigation.map((item) =>
-                  <li>
-                    <a href="#">{item.name}</a>
+          <div className='w-1/2 flex flex-row justify-between'>
+            <div className='w-1/3'>
+              <h6 className='text-xl text-(--color-title) font-medium pb-6'>Links</h6>
+              <ul className='flex flex-col gap-4'>
+                {navigation.map((item, index) =>
+                  <li key={index}>
+                    <a href="#" className='hover:text-(--color-title) duration-300 ease-in'>{item.name}</a>
                   </li>
                 )}
               </ul>
             </div>
-            <div>
-              <h6>Products</h6>
-              <ul>
-                <li><a href="#">Men</a></li>
-                <li><a href="#">Men</a></li>
-                <li><a href="#">Men</a></li>
-                <li><a href="#">Men</a></li>
-                <li><a href="#">Men</a></li>
+
+            <div className='w-1/3'>
+              <h6 className='text-xl text-(--color-title) font-medium pb-6'>Products</h6>
+              <ul className='flex flex-col gap-4'>
+                <li><a href="#" className='hover:text-(--color-title) duration-300 ease-in'>Men</a></li>
+                <li><a href="#" className='hover:text-(--color-title) duration-300 ease-in'>Accesories</a></li>
+                <li><a href="#" className='hover:text-(--color-title) duration-300 ease-in'>Women</a></li>
+                <li><a href="#" className='hover:text-(--color-title) duration-300 ease-in'>Outerwear</a></li>
+                <li><a href="#" className='hover:text-(--color-title) duration-300 ease-in'>Clothing</a></li>
               </ul>
             </div>
-            <div>
-              <h6>Contact</h6>
-              <ul>3
-                <li><a href="">eabelva@gmail.com</a></li>
-                <li><address>Perú-Av. Alfredo Benavides 778, Miraflores 15074</address></li>
+
+            <div className='w-1/3'>
+              <h6 className='text-xl text-(--color-title) font-medium pb-6'>Contact</h6>
+              <ul className='flex flex-col gap-4'>
+                <li><a href="" className='hover:text-(--color-title) duration-300 ease-in'>eabelva@gmail.com</a></li>
+                <li><address>Perú - Av. Alfredo Benavides 778, Miraflores 15074</address></li>
               </ul>
             </div>
           </div>
